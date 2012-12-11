72796441690e00d5d40b0a0d3157d12d704948a0
nanpin70 <nanpin70@gmail.com>
2012-12-11 21:20:11 +0900

    Update imj.js
    aaa

--- imj.js
+++ imj.js
@@ -1,8 +1,8 @@
 $(function(){
-  $(".entry").each(function() {
-	  $.get($(this).attr("href"), function(data) {
-      var image = $(data).find(".img_thumb");
-		  $("." + $(this).attr("class") + " > .tmb").append(image);
-    });
-  });
+	$(".entry").each(function() {
+  		$.get($(this).attr("href"), function(data, textStatus, jqXHR) {
+      		var image = $(data).find(".img_thumb");
+			$(".entry > .noload:first").append(image).toggleClass("noload");
+		});
+	});
 });
