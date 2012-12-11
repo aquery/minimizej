065b3853d4dad71fe7c262184faf4adb5d7a2d06
nanpin70 <nanpin70@gmail.com>
2012-12-11 21:38:56 +0900

    Update imj.js
    multi image 

--- imj.js
+++ imj.js
@@ -1,8 +1,11 @@
 $(function(){
 	$(".entry").each(function() {
   		$.get($(this).attr("href"), function(data, textStatus, jqXHR) {
-      		var image = $(data).find(".img_thumb");
-			$(".entry > .noload:first").append(image).toggleClass("noload");
+      		var images = $(data).find(".img_thumb");
+      		if ($(images).length > 0) {
+				var image = $(images).eq(Math.floor(Math.random() * $(images).length));
+				$(".entry > .noload:first").append(image).toggleClass("noload");
+			}
 		});
 	});
 });
